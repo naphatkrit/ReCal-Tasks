33c5792503dd36b7b84bfa3baab5bc48a5e25649
naphatkrit <naphat.krit@gmail.com>
2015-04-20 20:13:15 -0400

    migration code

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ReCal-Tasks
+
+## Migrations
+### Running Migration
+Initialize an empty database, then run this command (do **not** try to run `sync()` on the `sequelize` object itself.).
+
+```
+./node_modules/.bin/sequelize db:migrate
+```
+
+### Rolling Back Migrations
+
+Run the command:
+
+```
+./node_modules/.bin/sequelize db:migrate:undo
+```
+
+### More Info
+See [here](http://docs.sequelizejs.com/en/latest/docs/migrations/).
--- models/task.js
+++ models/task.js
@@ -1,3 +1,17 @@
+var Sequelize = require('sequelize');
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('Task', {});
+    return sequelize.define('Task', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        status: {
+            type: Sequelize.ENUM,
+            values: ["complete", "incomplete"],
+            allowNull: false,
+            defaultValue: "incomplete"
+        }
+    });
 };
