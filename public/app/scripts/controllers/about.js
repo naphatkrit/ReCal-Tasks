3d459a448987f04caf06639410ff3fe9c4bdc1d4
Dyland Xue <dylandxue2012@gmail.com>
2015-05-05 15:40:04 -0400

    initialized angular project

---
+++ public/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name recalTasksApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the recalTasksApp
+ */
+angular.module('recalTasksApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ public/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name recalTasksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the recalTasksApp
+ */
+angular.module('recalTasksApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
